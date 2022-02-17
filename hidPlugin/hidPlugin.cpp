861b9443513bb5499b3452cc5d1a1ce08f758237
Pornthep Preechayasomboon <prnthp@uw.edu>
2022-02-16 16:58:58 -0800

    Support more than 1 device

--- hidPlugin/hidPlugin.cpp
+++ hidPlugin/hidPlugin.cpp
@@ -4,63 +4,59 @@
 #include "hidPlugin.h"
 #include "hidapi.h"
 #include <math.h>
+#include <set>
+#include <map>
+
+std::map<std::tuple<int, int>, hid_device*> devices;
 
 extern "C" 
 {
-    hid_device* device_handle;
-    int latest_vid = 0;
-    int latest_pid = 0;
-    int latest_usage_page = 0;
+    int r;
+    hid_device* invalid_handle = NULL;
 
-    HIDPLUGIN_API int Connect(int VID, int PID, int usage_page)
+    HIDPLUGIN_API hid_device* Connect(int VID, int PID, int usage_page)
     {
-        latest_vid = VID;
-        latest_pid = PID;
-        latest_usage_page = usage_page;
+        // USAGE_PAGE = 0xFF00
+        // USAGE = 0x0100
+        std::tuple<int, int> vidpid = std::make_tuple(VID, PID);
 
-        if (device_handle != nullptr)
-        {
-            hid_close(device_handle);
-        }
+        hid_device* handle = invalid_handle;
 
-        int r;
-        r = hid_init();
-        if (r < 0)
+        for (auto& device : devices)
         {
-            return r;
+            if (device.first == vidpid)
+            {
+                return device.second;
+            }
         }
 
-        device_handle = hid_open(VID, PID, usage_page);
-        if (device_handle == nullptr)
+        handle = hid_open(VID, PID, NULL);
+
+        if (handle)
         {
-            return -2;
+            devices.insert(std::make_pair(vidpid, handle));
         }
-        else
-        {
-            return 1;
-        }
+
+        return handle;
     }
 
     unsigned char buf[65];
     char ret_buf[64];
 
-    HIDPLUGIN_API void* Receive()
+    HIDPLUGIN_API void* Receive(hid_device* device_handle)
     {
         memset(ret_buf, 0, 64);
+        int r = -1;
 
-        if (device_handle == nullptr)
+        for (auto& device : devices)
         {
-            int connect_r = Connect(latest_vid, latest_pid, latest_usage_page);
-
-            if (connect_r < 0)
+            if (device.second == device_handle)
             {
-                return ret_buf;
+                r = hid_read_timeout(device_handle, buf, 64, 1);
             }
         }
 
-        int r;
-        r = hid_read_timeout(device_handle, buf, 64, 1);
-
+        // If recv fail, we set all to -1
         if (r <= 0)
         {
             for (int i = 0; i < 64; i++)
@@ -73,77 +69,62 @@ extern "C"
         return ret_buf;
     }
 
-    HIDPLUGIN_API int Send(char* buf, int num_bytes)
+    HIDPLUGIN_API int Send(hid_device* device_handle, char* buf, int num_bytes)
     {
-        if (device_handle == nullptr)
-        {
-            int connect_r = Connect(latest_vid, latest_pid, latest_usage_page);
-
-            if (connect_r < 0)
-            {
-                return -1;
-            }
-        }
-
         int r = 0;
-        int num_packets = ceil((float)num_bytes / 61);
-        unsigned char write_buf[65];
-        memset(write_buf, 0, 65);
-        write_buf[1] = 0xAB; // Signature
-        write_buf[2] = num_bytes >> 8;
-        write_buf[3] = num_bytes;
 
-        int idx = 0;
-        for (int p = 0; p < num_packets; p++)
+        for (auto& device : devices)
         {
-            for (int i = 4; i < 65; i++)
+            if (device.second == device_handle)
             {
-                if (idx >= num_bytes)
+                int num_packets = ceil((float)num_bytes / 61);
+                unsigned char write_buf[65];
+                memset(write_buf, 0, 65);
+                write_buf[1] = 0xAB; // Signature
+                write_buf[2] = num_bytes >> 8;
+                write_buf[3] = num_bytes;
+
+                int idx = 0;
+                for (int p = 0; p < num_packets; p++)
                 {
-                    write_buf[i] = 0;
+                    for (int i = 4; i < 65; i++)
+                    {
+                        if (idx >= num_bytes)
+                        {
+                            write_buf[i] = 0;
+                        }
+                        else
+                        {
+                            write_buf[i] = buf[idx];
+                        }
+                        idx++;
+                    }
+                    r = hid_write(device_handle, write_buf, 65);
                 }
-                else
-                {
-                    write_buf[i] = buf[idx];
-                }
-                idx++;
+                return r;
             }
-            r = hid_write(device_handle, write_buf, 65);
         }
-        return r;
+
+        return -1;
     }
 
-    HIDPLUGIN_API void Disconnect()
+    HIDPLUGIN_API void Disconnect(hid_device* device_handle)
     {
-        if (device_handle != nullptr)
+        for (auto device = devices.begin(); device != devices.end();)
         {
-            hid_close(device_handle);
-            device_handle = nullptr;
+            if (device->second == device_handle)
+            {
+                hid_close(device_handle);
+                devices.erase(device++);
+            }
+            else
+            {
+                ++device;
+            }
         }
+
         return;
     }
-
-    HIDPLUGIN_API int TestCommand()
-    {
-        if (device_handle != nullptr)
-        {
-            return latest_usage_page;
-        }
-        else
-        {
-            return 0;
-        }
-    }
-}
-
-
-// This is an example of an exported variable
-HIDPLUGIN_API int nhidPlugin=0;
-
-// This is an example of an exported function.
-HIDPLUGIN_API int fnhidPlugin(void)
-{
-    return 0;
 }
 
 // This is the constructor of a class that has been exported.
